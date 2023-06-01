38aefb3c50a8298fb4feaf1921d7d826f2631d6a
Ruby <rubyfredrickson@gmail.com>
2023-05-31 19:56:19 -0500

    Added questions and limited functionality

--- Develop/index.js
+++ Develop/index.js
@@ -1,19 +1,71 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
+        name: "title",
+        message: "What is the name of your project?",
+    },
+
+    {
+        type: "input",
+        name: "description",
+        message: "Describe your project here:",
+    },
+
+    {
+        type: "input",
+        name: "installation",
+        message: "How can the user install this project?",
+    },
+
+    {
+        type: "input",
+        name: "usage",
+        message: "How can the user use this project?",
+    },
+
+    {
+        type: "Checkbox",
+        name: "lisence",
+        message: "What liscence does this project use?",
+        choices: ['MIT', 'Other'],
+    },
+
+    {
+        type: "input",
+        name: "contributing",
+        message: "Who worked on this project?",
+    },
+
+    {
+        type: "input",
         name: "test",
-        message: "Type now.",
+        message: "How can the user test this project?",
+    },
+
+    {
+        type: "input",
+        name: "questions",
+        message: "What is your github username?",
     },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync();
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        console.log();
+        writeToFile();
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,15 @@
+const generateMarkdown = require('../index');
+let coolBadge = "Insert Badge Info Here"
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(questions.license = 'MIT'){
+    coolBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  } else {
+    coolBadge = ""
+  }
+
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -11,9 +20,38 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(questions) {
+  return `# ${questions.title}
+  ## Description
+  ${questions.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [Lisence](#Lisence)
+  - [Contributing](#Contributing)
+  - [Test](#Test)
+  - [Questions](#Questions)
+  
+  ## Installation
+  ${questions.installation}
 
+  ## Usage
+  ${questions.Usage}
+
+  ## Lisence
+
+  ${coolBadge}
+
+  ## Contributing
+  ${questions.contributing}
+  
+  ## Test
+  ${questions.Usage}
+  
+  ## Questions
+  https://github.com/  ${data.questions}
 `;
 }
 
